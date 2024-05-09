e3fdfe2de8dd64dec9df66012f44f4b0b4c31eeb
antmusumba <musumbaantony3@gmail.com>
2024-05-09 15:17:42 +0300

    writing to another file and correcting if there is an error in the original

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"reflect"
+)
+func main(){
+	if len(os.Args) != 3 {
+		fmt.Println("Not enough arguments")
+		return
+	}
+	args := os.Args
+
+	inputfile:= args[1]
+	outputfile:= args[2]
+
+	 data1, err := os.ReadFile((inputfile))
+	 if err != nil {
+		fmt.Println("Error reading file:",err)
+		return
+	 }
+	 //fmt.Println(string(data1))
+	 data2 := string(data1)
+	 
+	 expected := "package main\nimport \"fmt\"\n\nfunc main() {\n   fmt.Println(\"Hello, World!\")\n}"
+	 if !reflect.DeepEqual(expected,data2) {
+		data2 = "package main\nimport \"fmt\"\n\nfunc main() {\n   fmt.Println(\"Hello, World!\")\n}"
+	 } else {
+		fmt.Println("Well formatted")
+
+	 }
+	 data3 :=[]byte(data2)
+	 os.WriteFile(outputfile,data3,0644)
+	//  if err != nil {}
+	// 	fmt.Println("error writing file:",err)
+	//  }
+
+	 
+	
+
+}
